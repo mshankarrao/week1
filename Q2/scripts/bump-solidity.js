c31fccb8837ce2dc6359d4a053d4427c3876384c
Shankar Rao Mata <mshankarrao1970@gmail.com>
2022-05-11 21:44:53 -0400

    testcases for 5th question

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -10,3 +10,9 @@ bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+
+let content1 = fs.readFileSync("./contracts/HelloWorldVerifier.sol", { encoding: 'utf-8' });
+let bumped1 = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped1 = bumped1.replace(verifierRegex, 'contract HelloWorldVerifier');
+
+fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
